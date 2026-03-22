9609904b97fd673e247440ccbbfbae3b996f3717
Alessio270403 <marcuzzial@gmail.com>
2026-03-22 17:30:20 +0100

    compiler(parser stage): local sync struct tests
    a

--- compiler/tests/stages/frontend/parser/errors_synchronization/parser_errors_synchronization_base.h
+++ compiler/tests/stages/frontend/parser/errors_synchronization/parser_errors_synchronization_base.h
@@ -3,10 +3,13 @@
 #include <gtest/gtest.h>
 #include <memory>
 #include <vector>
+#include <functional>
+#include <algorithm>
 #include "stages/frontend/lexer/lexer_stage.h"
 #include "stages/frontend/parser/parser_stage.h"
 #include "compiler_context/compiler_context.h"
 #include "errors/valuascript_exception.h"
+#include "stages/frontend/parser/ast.h"
 
 using namespace valuascript::compiler;
 
@@ -20,6 +23,7 @@ struct ParserErrorsSynchronizationTestCase {
     std::string test_name;
     std::string source_code;
     std::vector<ExpectedParserError> expected_errors;
+    std::function<void(const Program&)> verify_ast = nullptr;
 };
 
 class ParserErrorsSynchronizationBase : public testing::TestWithParam<ParserErrorsSynchronizationTestCase> {
@@ -39,20 +43,21 @@ protected:
         auto lexer_artifacts = initial_artifacts;
         lexer_artifacts.push_back(lexer.run(*context, initial_artifacts));
 
+        CompilerStageArtifact ast_artifact;
         ASSERT_NO_THROW({
-            parser.run(*context, lexer_artifacts);
-            }) << "Parser threw an exception even though fail_fast was set to false.";
+            ast_artifact = parser.run(*context, lexer_artifacts);
+        }) << "Parser threw an exception even though fail_fast was set to false.";
 
         const auto &actual_errors = context->diagnostics.get_errors();
 
-        ASSERT_EQ(actual_errors.size(), param.expected_errors.size())
+        EXPECT_EQ(actual_errors.size(), param.expected_errors.size())
             << "Mismatch in the number of collected errors.\n"
             << "Expected " << param.expected_errors.size() << ", but got " << actual_errors.size();
 
-        for (size_t i = 0; i < actual_errors.size(); ++i) {
+        size_t errors_to_check = std::min(actual_errors.size(), param.expected_errors.size());
+        for (size_t i = 0; i < errors_to_check; ++i) {
             const auto &actual = actual_errors[i];
             const auto &expected = param.expected_errors[i];
-            std::cout << actual.what();
 
             EXPECT_EQ(actual.get_code(), expected.code)
                 << "Error [" << i << "] Code mismatch.\nExpected Code: " << static_cast<int>(expected.code)
@@ -64,6 +69,16 @@ protected:
 
             EXPECT_EQ(actual.get_span().column_end, expected.column)
                 << "Error [" << i << "] Column mismatch for error: " << actual.what();
+        }
+
+        if (param.verify_ast) {
+            ASSERT_EQ(ast_artifact.code, CompilerStageArtifactCode::Ast)
+                << "Parser did not return an AST artifact.";
+
+            auto ast = std::any_cast<std::shared_ptr<Program>>(ast_artifact.data);
+            ASSERT_NE(ast, nullptr) << "Parsed AST is null.";
+
+            param.verify_ast(*ast);
         }
     }
 };
---
+++ compiler/tests/stages/frontend/parser/errors_synchronization/parser_struct_errors_synchronization_tests.cpp
@@ -0,0 +1,161 @@
+#include <gtest/gtest.h>
+#include "parser_errors_synchronization_base.h"
+
+using Err = valuascript::compiler::ValuascriptErrorCode;
+
+namespace {
+    const StructDefinition *ExpectRecoveredStruct(const Program &ast, const std::string &expected_name) {
+        EXPECT_EQ(ast.execution_steps.size(), 1) << "Expected 'let a = 1' to survive.";
+
+        if (ast.struct_definitions.empty()) return nullptr;
+
+        EXPECT_EQ(ast.struct_definitions.size(), 1);
+        const auto *struct_def = ast.struct_definitions.front().get();
+        EXPECT_EQ(struct_def->name, expected_name);
+        return struct_def;
+    }
+
+    void ExpectStructFields(const StructDefinition *struct_def,
+                            const std::vector<std::pair<std::string, std::string> > &expected_fields) {
+        ASSERT_NE(struct_def, nullptr) << "Struct definition was null!";
+        ASSERT_EQ(struct_def->fields.size(), expected_fields.size()) << "Recovered field count mismatch!";
+
+        for (size_t i = 0; i < expected_fields.size(); ++i) {
+            EXPECT_EQ(struct_def->fields[i].first, expected_fields[i].first)
+                << "Field name mismatch at index " << i;
+
+            ASSERT_NE(struct_def->fields[i].second, nullptr)
+                << "Type annotation missing at index " << i;
+
+            EXPECT_EQ(struct_def->fields[i].second->name, expected_fields[i].second)
+                << "Type name mismatch at index " << i;
+        }
+    }
+
+    auto ExpectNoStructs() {
+        return [](const Program &ast) {
+            ASSERT_EQ(ast.struct_definitions.size(), 0);
+            ASSERT_EQ(ast.execution_steps.size(), 1);
+        };
+    }
+
+    auto ExpectStruct(std::string name, std::vector<std::pair<std::string, std::string> > fields = {}) {
+        return [name = std::move(name), fields = std::move(fields)](const Program &ast) {
+            auto s = ExpectRecoveredStruct(ast, name);
+            ExpectStructFields(s, fields);
+        };
+    }
+}
+
+class StructParserSynchronizationTest : public ParserErrorsSynchronizationBase {
+};
+
+TEST_P(StructParserSynchronizationTest, CollectsMultipleSyntaxErrorsAtCorrectLocations) {
+    run_parser_and_check_errors(GetParam());
+}
+
+INSTANTIATE_TEST_SUITE_P(
+    ParserExhaustiveStressTests,
+    StructParserSynchronizationTest,
+    ::testing::Values(
+        ParserErrorsSynchronizationTestCase{
+        "no_name_struct_empty_ast",
+        "struct { id: int }\n"
+        "let a = 1\n",
+        { {Err::ExpectedStructName, 1, 9} },
+        ExpectNoStructs()
+        },
+        ParserErrorsSynchronizationTestCase{
+        "no_left_brace_struct_empty_ast",
+        "struct Test id: int }\n"
+        "let a = 1\n",
+        { {Err::ExpectedBraceInStructDefinition, 1, 15} },
+        ExpectNoStructs()
+        },
+        ParserErrorsSynchronizationTestCase{
+        "no_right_brace_struct_empty_ast",
+        "struct Test { id: int \n"
+        "let a = 1\n",
+        { {Err::ExpectedRightBraceAfterStructBody, 1, 23} },
+        ExpectNoStructs()
+        },
+        ParserErrorsSynchronizationTestCase{
+        "no_colon_empty_struct_in_ast",
+        "struct Test { id int } \n"
+        "let a = 1\n",
+        { {Err::ExpectedColonAfterStructFieldName, 1, 21} },
+        ExpectStruct("Test")
+        },
+        ParserErrorsSynchronizationTestCase{
+        "no_field_name_empty_struct_in_ast",
+        "struct Test {  : int } \n"
+        "let a = 1\n",
+        { {Err::ExpectedStructFieldName, 1, 17} },
+        ExpectStruct("Test")
+        },
+        ParserErrorsSynchronizationTestCase{
+        "no_commas_all_fields_struct_in_ast",
+        "struct Test { host: string port: int speed: int mode: string }\n"
+        "let a = 1\n",
+        {
+        {Err::ExpectedCommaSeparatorInStruct, 1, 32},
+        {Err::ExpectedCommaSeparatorInStruct, 1, 43},
+        {Err::ExpectedCommaSeparatorInStruct, 1, 53},
+        },
+        ExpectStruct("Test", {
+            {"host", "string"},
+            {"port", "int"},
+            {"speed", "int"},
+            {"mode", "string"}
+            })
+        },
+        ParserErrorsSynchronizationTestCase{
+        "no_type_all_other_fields_struct_in_ast",
+        "struct Test { host: string port: int speed: int mode: }\n"
+        "let a = 1\n",
+        {
+        {Err::ExpectedCommaSeparatorInStruct, 1, 32},
+        {Err::ExpectedCommaSeparatorInStruct, 1, 43},
+        {Err::ExpectedCommaSeparatorInStruct, 1, 53},
+        {Err::MissingTypeAnnotation, 1, 56},
+        },
+        ExpectStruct("Test", {
+            {"host", "string"},
+            {"port", "int"},
+            {"speed", "int"}
+            })
+        },
+        ParserErrorsSynchronizationTestCase{
+        "no_colon_all_other_fields_struct_in_ast",
+        "struct Test { host: string port: int speed int mode: string }\n"
+        "let a = 1\n",
+        {
+        {Err::ExpectedCommaSeparatorInStruct, 1, 32},
+        {Err::ExpectedCommaSeparatorInStruct, 1, 43},
+        {Err::ExpectedColonAfterStructFieldName, 1, 47}
+        },
+        ExpectStruct("Test", {
+            {"host", "string"},
+            {"port", "int"}
+            })
+        },
+        ParserErrorsSynchronizationTestCase{
+        "no_colon_plus_second_last_comma_all_other_fields_struct_in_ast",
+        "struct Test { host: string port: int speed int, mode: string }\n"
+        "let a = 1\n",
+        {
+        {Err::ExpectedCommaSeparatorInStruct, 1, 32},
+        {Err::ExpectedCommaSeparatorInStruct, 1, 43},
+        {Err::ExpectedColonAfterStructFieldName, 1, 47}
+        },
+        ExpectStruct("Test", {
+            {"host", "string"},
+            {"port", "int"},
+            {"mode", "string"}
+            })
+        }
+    ),
+    [](const ::testing::TestParamInfo<ParserErrorsSynchronizationTestCase>& info) {
+    return info.param.test_name;
+    }
+);
