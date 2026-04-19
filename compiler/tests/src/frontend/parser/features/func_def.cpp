7d0badd1ba26c0ade2f65b5958cad755cfe01aed
Alessio270403 <marcuzzial@gmail.com>
2026-04-19 19:57:48 +0200

    tests(compiler): func def

---
+++ compiler/tests/src/frontend/parser/features/func_def.cpp
@@ -0,0 +1,159 @@
+#include "frontend/parser/helpers/parser_test_base.h"
+
+namespace valuascript::compiler::test
+{
+    class FunctionDefinitionSuccessPathTest : public ParserTestBase
+    {
+    };
+
+    TEST_F(FunctionDefinitionSuccessPathTest, MinimalFunction)
+    {
+        ExpectValidParse(
+            "func f() -> void {}",
+            ProgramSpec{
+                .functions = {
+                    IsFunctionDef("f", {}, {}, {IsType("void")}, {})
+                }
+            }
+        );
+    }
+
+    TEST_F(FunctionDefinitionSuccessPathTest, MultipleParameters)
+    {
+        ExpectValidParse(
+            "func add(a: int, b: int) -> int {}",
+            ProgramSpec{
+                .functions = {
+                    IsFunctionDef("add", {}, {
+                                      ParamSpec{"a", {}, IsType("int")},
+                                      ParamSpec{"b", {}, IsType("int")}
+                                  }, {IsType("int")}, {})
+                }
+            }
+        );
+    }
+
+    TEST_F(FunctionDefinitionSuccessPathTest, SingleDefaultParameterValue)
+    {
+        ExpectValidParse(
+            "func f(a: int, b: bool = true) -> void {}",
+            ProgramSpec{
+                .functions = {
+                    IsFunctionDef("f", {}, {
+                                      ParamSpec{"a", {}, IsType("int")},
+                                      ParamSpec{"b", {}, IsType("bool"), IsBoolean(true)}
+                                  }, {IsType("void")}, {})
+                }
+            }
+        );
+    }
+
+    TEST_F(FunctionDefinitionSuccessPathTest, DefaultParameterValues)
+    {
+        ExpectValidParse(
+            "func f(a: int = 1, b: bool = true) -> void {}",
+            ProgramSpec{
+                .functions = {
+                    IsFunctionDef("f", {}, {
+                                      ParamSpec{"a", {}, IsType("int"), IsNumber("1")},
+                                      ParamSpec{"b", {}, IsType("bool"), IsBoolean(true)}
+                                  }, {IsType("void")}, {})
+                }
+            }
+        );
+    }
+
+    TEST_F(FunctionDefinitionSuccessPathTest, MultipleReturnTypes)
+    {
+        ExpectValidParse(
+            "func f() -> int, string {}",
+            ProgramSpec{
+                .functions = {
+                    IsFunctionDef("f", {}, {}, {
+                                      IsType("int"),
+                                      IsType("string")
+                                  }, {})
+                }
+            }
+        );
+    }
+
+    TEST_F(FunctionDefinitionSuccessPathTest, MultipleReturnValues)
+    {
+        ExpectValidParse(
+            "func f() -> int { return 1, 2}",
+            ProgramSpec{
+                .functions = {
+                    IsFunctionDef("f", {}, {}, {
+                                      IsType("int"),
+                                  }, {
+                                      IsReturn({
+                                          IsNumber("1"),
+                                          IsNumber("2"),
+                                      })
+                                  })
+                }
+            }
+        );
+    }
+
+    TEST_F(FunctionDefinitionSuccessPathTest, FunctionWithDocstring)
+    {
+        ExpectValidParse(
+            "func f() -> void {\n"
+            "  \"\"\"This is a docstring\"\"\"\n"
+            "}",
+            ProgramSpec{
+                .functions = {
+                    IsFunctionDef("f", {}, {}, {IsType("void")}, {}, "\"\"\"This is a docstring\"\"\"")
+                }
+            }
+        );
+    }
+
+    TEST_F(FunctionDefinitionSuccessPathTest, FunctionWithBodyStatements)
+    {
+        ExpectValidParse(
+            "func f() -> void {\n"
+            "  let x = 1\n"
+            "  return x\n"
+            "}",
+            ProgramSpec{
+                .functions = {
+                    IsFunctionDef("f", {}, {}, {IsType("void")}, {
+                                      IsAssignment({}, {{"x"}}, IsNumber("1")),
+                                      IsReturn({IsIdentifier("x")})
+                                  })
+                }
+            }
+        );
+    }
+
+    TEST_F(FunctionDefinitionSuccessPathTest, MultilineFormatting)
+    {
+        ExpectValidParse(
+            "func long_function_name(\n"
+            "  param_one: int,\n"
+            "  param_two: string\n"
+            ") -> \n"
+            "  bool, \n"
+            "  decimal \n"
+            "{\n"
+            "  return true, 1.0\n"
+            "}",
+            ProgramSpec{
+                .functions = {
+                    IsFunctionDef("long_function_name", {}, {
+                                      ParamSpec{"param_one", {}, IsType("int")},
+                                      ParamSpec{"param_two", {}, IsType("string")}
+                                  }, {
+                                      IsType("bool"),
+                                      IsType("decimal")
+                                  }, {
+                                      IsReturn({IsBoolean(true), IsNumber("1.0")})
+                                  })
+                }
+            }
+        );
+    }
+}
--- compiler/tests/src/frontend/parser/parsing/function_definition.cpp
+++ compiler/tests/src/frontend/parser/parsing/function_definition.cpp
@@ -4,105 +4,31 @@
 
 using namespace valuascript::compiler;
 
-namespace valuascript::compiler::test {
-    struct FunctionHappyParam {
-        std::string test_name;
-        std::string source_code;
-        size_t expected_param_count;
-        size_t expected_return_type_count;
-        size_t expected_body_statements;
-        bool expects_docstring;
-    };
-
-    class FunctionHappyPathTest : public AstBaseTest,
-                                  public testing::WithParamInterface<FunctionHappyParam> {
-    };
-
-    TEST_P(FunctionHappyPathTest, ParsesSuccessfully) {
-        const FunctionHappyParam &param = GetParam();
-
-        std::shared_ptr<Program> ast;
-        EXPECT_NO_THROW({
-            ast = parse_code(param.source_code);
-            }) << "Parser threw an exception on valid function test: " << param.test_name;
-
-        if (ast) {
-            ASSERT_EQ(ast->function_definitions.size(), 1) << "Expected exactly 1 function in AST.";
-            EXPECT_EQ(ast->execution_steps.size(), 0);
-            EXPECT_EQ(ast->directives.size(), 0);
-
-            auto &func = ast->function_definitions[0];
-            EXPECT_EQ(func->parameters.size(), param.expected_param_count) << "Parameter count mismatch.";
-            EXPECT_EQ(func->return_types.size(), param.expected_return_type_count) << "Return type count mismatch.";
-            EXPECT_EQ(func->body.size(), param.expected_body_statements) << "Body statement count mismatch.";
-            EXPECT_EQ(func->docstring.has_value(), param.expects_docstring) << "Docstring presence mismatch.";
-        }
-    }
-
-    INSTANTIATE_TEST_SUITE_P(
-        ParserStageTest,
-        FunctionHappyPathTest,
-        testing::Values(
-            FunctionHappyParam{"minimal_func", "func test() -> scalar {}", 0, 1, 0, false},
-            FunctionHappyParam{"single_param", "func test(a: scalar) -> scalar {}", 1, 1, 0, false},
-            FunctionHappyParam{"multi_param", "func test(a: scalar, b: boolean) -> scalar {}", 2, 1, 0, false},
-            FunctionHappyParam{"generic_param", "func process(data: vector<matrix>) -> scalar {}", 1, 1, 0, false},
-            FunctionHappyParam{"nested_generic_param", "func process(data: vector<vector<scalar>>) -> scalar {}", 1, 1,
-            0,
-            false},
-            FunctionHappyParam{"single_param_default", "func test(a: scalar = 1) -> scalar {}", 1, 1, 0, false},
-            FunctionHappyParam{"multi_param_default", "func test(a: scalar = 1, b: boolean = true) -> scalar {}", 2, 1,
-            0, false},
-            FunctionHappyParam{"generic_param_default", "func process(data: vector<matrix> = []) -> scalar {}", 1, 1, 0,
-            false},
-            FunctionHappyParam{"nested_generic_param_default",
-            "func process(data: vector<vector<scalar>> = [[1]]) -> scalar {}", 1, 1,
-            0,
-            false},
-            FunctionHappyParam{"multi_param_default_on_only_one", "func test(a: scalar, b: boolean = true) -> scalar {}"
-            , 2, 1, 0, false},
-            FunctionHappyParam{"tuple_return", "func bounds() -> (scalar, scalar) {}", 0, 1, 0, false},
-            FunctionHappyParam{"tuple_return_generic", "func bounds() -> (scalar, vector<scalar>) {}", 0, 1, 0, false},
-            FunctionHappyParam{"tuple_param", "func bounds(a: (scalar, scalar, Custom)) -> (scalar, Custom) {}", 1, 1, 0
-            , false},
-            FunctionHappyParam{"tuple_param_generic",
-            "func bounds(a: (scalar, vector<scalar>, Custom)) -> (scalar, Custom) {}", 1, 1, 0, false},
-            FunctionHappyParam{"param_generic", "func test(a: Input<A, B>) -> Result<T, E> {}", 1, 1, 0, false},
-            FunctionHappyParam{"tuple_and_struct_param",
-            "func bounds(a: (scalar, scalar, Custom), b: Other) -> (scalar, Custom) {}", 2, 1, 0, false},
-            FunctionHappyParam{"tuple_and_struct_return",
-            "func bounds(a: (scalar, scalar, Custom), b: Other) -> (scalar, Custom), Other {}", 2, 2, 0, false},
-            FunctionHappyParam{"dict_literal_return", "func dict() -> dict { return { cagr: 1, yrs: 10 } }", 0, 1, 1,
-            false},
-            FunctionHappyParam{"docstring", "func test() -> scalar { \"\"\"Calculates something.\"\"\" }", 0, 1, 0, true
-            },
-            FunctionHappyParam{"body_statements", "func test() -> scalar { let a = 1 \n return a }", 0, 1, 2, false},
-            FunctionHappyParam{"kitchen_sink",
-            "func full(v: vector<scalar>, b: boolean) -> (vector<scalar>, boolean) { \"\"\"Docs\"\"\" let out = v \n return out }"
-            , 2, 1, 2, true}
-        ),
-        [](const testing::TestParamInfo<FunctionHappyParam>& info) {
-        return info.param.test_name;
-        }
-    );
-
-    struct FunctionSadParam {
+namespace valuascript::compiler::test
+{
+    struct FunctionSadParam
+    {
         std::string test_name;
         std::string source_code;
         ValuascriptErrorCode expected_error;
     };
 
     class FunctionSadPathTest : public AstBaseTest,
-                                public testing::WithParamInterface<FunctionSadParam> {
+                                public testing::WithParamInterface<FunctionSadParam>
+    {
     };
 
-    TEST_P(FunctionSadPathTest, ThrowsCorrectSyntaxError) {
-        const FunctionSadParam &param = GetParam();
+    TEST_P(FunctionSadPathTest, ThrowsCorrectSyntaxError)
+    {
+        const FunctionSadParam& param = GetParam();
 
-        try {
+        try
+        {
             parse_code(param.source_code);
             FAIL() << "Parser should have thrown an exception for test: " << param.test_name;
-        } catch (const ValuaScriptException &e) {
+        }
+        catch (const ValuaScriptException& e)
+        {
             EXPECT_EQ(e.get_category(), ValuascriptErrorCategory::Syntax)
                 << "Category mismatch on test: " << param.test_name;
             EXPECT_EQ(e.get_code(), param.expected_error)
@@ -159,7 +85,8 @@ namespace valuascript::compiler::test {
             FunctionSadParam{"top_level_declaration_in_func_4",
             "func test(a: s) -> s { return 1 \n let a = b()\n func other() -> scalar {}\n }", ValuascriptErrorCode::
             TopLevelDeclarationNotAllowedHere},
-            FunctionSadParam{"single_param_default", "func test(a: scalar = ) -> scalar {}", ValuascriptErrorCode::MissingDefaultParameterValue},
+            FunctionSadParam{"single_param_default", "func test(a: scalar = ) -> scalar {}", ValuascriptErrorCode::
+            MissingDefaultParameterValue},
             FunctionSadParam{"multi_param_default", "func test(a: scalar = 1, b: boolean) -> scalar {}",
             ValuascriptErrorCode::NonDefaultParameterAfterDefault}
         ),
